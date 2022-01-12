6ce977099ebbd261cfe9aee2656f7e0fe259fa11
vallenov <a2lenov@mail.ru>
2022-01-13 00:46:33 +0300

    Доработана функция снятия метрики температуры Добавлена блокировка постоянных сообщений

--- monitor_classes.py
+++ monitor_classes.py
@@ -1,11 +1,14 @@
 import subprocess as sb
 
+
 class Monitor:
+    block_message = {}
 
     @staticmethod
-    def get_temperature() -> int:
+    def get_temperature() -> str and int:
+        name = sb.check_output("uname -n", shell=True).decode()[:-1]
         output = sb.check_output("cat `find /sys/ -name temp 2>/dev/null`", shell=True)
         temp_list = output.decode().split()
         temp_list = list(map(lambda t: int(t), temp_list))
         temp = (sum(temp_list) // len(temp_list)) // 1000
-        return temp
+        return name, temp
--- system_monitor.py
+++ system_monitor.py
@@ -1,17 +1,37 @@
 import requests
 import configparser
+import logging
+import time
 
 from monitor_classes import Monitor
 
-if __name__ == "__main__":
 
+def load_config():
     config = configparser.ConfigParser()
     config.read('system_monitor.ini', encoding='utf-8')
+    return config
 
-    MAX_TEMP = int(config.get('MAX_VALUES', 'max_temp'))
 
-    if Monitor.get_temperature() > MAX_TEMP:
-        data = dict()
-        data['to'] = config.get('CONTACT', 'telegram_name')
-        data['text'] = f'Температура малинки выше {MAX_TEMP}!'
-        requests.post(config.get('URL', 'message_server_address'), data=data, headers={'Connection':'close'})
+if __name__ == "__main__":
+    logging.basicConfig(filename='run.log',
+                        level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    conf = load_config()
+    while True:
+        try:
+            conf = load_config()
+
+            MAX_TEMP = int(conf.get('MAX_VALUES', 'max_temp'))
+
+            name, temp = Monitor.get_temperature()
+            if temp >= MAX_TEMP and Monitor.block_message.get('temperature', False) is False:
+                data = dict()
+                data['to'] = conf.get('CONTACT', 'telegram_name')
+                data['text'] = f'Температура {name} выше {MAX_TEMP}°C'
+                requests.post(conf.get('URL', 'message_server_address'), data=data, headers={'Connection': 'close'})
+                Monitor.block_message['temperature'] = True
+        except Exception as _ex:
+            logging.exception(f'Unrecognize exception {_ex}')
+
+        time.sleep(int(conf.get('SETTINGS', 'sleep_time')))
+
