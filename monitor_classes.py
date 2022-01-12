1def288881d288d14fe4ffca5957a590943c718c
vallenov <a2lenov@mail.ru>
2022-01-13 01:02:19 +0300

    Получение имени машины вынесено в отдельную функцию Добавлено описание функций

--- monitor_classes.py
+++ monitor_classes.py
@@ -5,10 +5,19 @@ class Monitor:
     block_message = {}
 
     @staticmethod
-    def get_temperature() -> str and int:
-        name = sb.check_output("uname -n", shell=True).decode()[:-1]
+    def get_name_of_machine() -> str:
+        """
+        Get name of the current machine
+        """
+        return sb.check_output("uname -n", shell=True).decode()[:-1]
+
+    @staticmethod
+    def get_temperature() -> int:
+        """
+        Get temperature of cores
+        """
         output = sb.check_output("cat `find /sys/ -name temp 2>/dev/null`", shell=True)
         temp_list = output.decode().split()
         temp_list = list(map(lambda t: int(t), temp_list))
         temp = (sum(temp_list) // len(temp_list)) // 1000
-        return name, temp
+        return temp
--- system_monitor.py
+++ system_monitor.py
@@ -23,15 +23,19 @@ if __name__ == "__main__":
 
             MAX_TEMP = int(conf.get('MAX_VALUES', 'max_temp'))
 
-            name, temp = Monitor.get_temperature()
+            temp = Monitor.get_temperature()
             if temp >= MAX_TEMP and Monitor.block_message.get('temperature', False) is False:
                 data = dict()
                 data['to'] = conf.get('CONTACT', 'telegram_name')
+                name = Monitor.get_name_of_machine()
                 data['text'] = f'Температура {name} выше {MAX_TEMP}°C'
                 requests.post(conf.get('URL', 'message_server_address'), data=data, headers={'Connection': 'close'})
+                logging.info('Send successful')
                 Monitor.block_message['temperature'] = True
+            elif temp < MAX_TEMP:
+                Monitor.block_message['temperature'] = False
         except Exception as _ex:
-            logging.exception(f'Unrecognize exception {_ex}')
+            logging.exception(f'Unrecognized exception {_ex}')
 
         time.sleep(int(conf.get('SETTINGS', 'sleep_time')))
 
