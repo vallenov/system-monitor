730e459feb9012f10235f8628125df0f613a98e0
vallenov <a2lenov@mail.ru>
2022-06-08 00:04:05 +0300

    Текущий функционал разнесен на отдельные модули

---
+++ check_functions.py
@@ -0,0 +1,83 @@
+import logging
+from monitor_classes import Monitor
+from ini_service import conf
+
+
+def check_temperature():
+    """
+    Проверка превышения заданной температуры
+    """
+
+    max_temp = int(conf.get('MAX_VALUES', 'max_temp'))
+
+    temp = Monitor.get_temperature()
+    if temp >= max_temp and Monitor.block_message.get('temperature', False) is False:
+        data = dict()
+        data['to'] = conf.get('CONTACT', 'telegram_name')
+        name = Monitor.get_name_of_machine()
+        data['text'] = f'Температура {name} сейчас: {temp}°C'
+        send_message(data)
+        logging.info('Send successful')
+        Monitor.block_message['temperature'] = True
+    elif temp < max_temp:
+        Monitor.block_message['temperature'] = False
+
+
+def check_ssh_connections():
+    """
+    Проверка ssh соединений
+    """
+    connections = Monitor.get_ssh_connections()
+    for conn in Monitor.block_message['ssh'].keys():
+        if conn not in connections:
+            Monitor.block_message['ssh'].pop(conn)
+    for conn in connections:
+        if Monitor.block_message.get('ssh', {}).get(conn, False) is False:
+            data = dict()
+            data['to'] = conf.get('CONTACT', 'telegram_name')
+            name = Monitor.get_name_of_machine()
+            data['text'] = str(f'Новое подключение к "{name}" с адреса {conn}.'
+                               f'Текущее количество подключений: {len(connections)}')
+            send_message(data)
+            Monitor.block_message['ssh'][conn] = True
+        elif len(connections) == 0:
+            Monitor.block_message['ssh'].clear()
+
+
+def check_used_space():
+    """
+    Проверка оставшегося свободного места на жестком диске
+    """
+    max_used_space = int(conf.get('MAX_VALUES', 'max_used_space'))
+    used_space = Monitor.get_used_space()
+    name = Monitor.get_name_of_machine()
+    for key, value in used_space.items():
+        if value > max_used_space and Monitor.block_message.get('used_space').get(key, False) is False:
+            data = dict()
+            data['to'] = conf.get('CONTACT', 'telegram_name')
+            data['text'] = f'Свободное место в "{key}" на сервере {name} заканчивается. Осталось {100 - value}%'
+            send_message(data)
+            Monitor.block_message['used_space'][key] = True
+        elif value < max_used_space:
+            Monitor.block_message['used_space'][key] = False
+
+
+def check_ip():
+    """
+    Проверка изменения IP-адреса
+    """
+    old_ip = None
+    if conf.has_option('SETTINGS', 'self_ip'):
+        old_ip = conf.get('SETTINGS', 'self_ip')
+    new_ip = Monitor.get_self_ip()
+    if old_ip != new_ip or old_ip is None:
+        conf.set('SETTINGS', 'self_ip', new_ip)
+        ini_save(conf)
+    else:
+        if new_ip == old_ip:
+            return
+    name = Monitor.get_name_of_machine()
+    data = dict()
+    data['to'] = conf.get('CONTACT', 'telegram_name')
+    data['text'] = f'Новый IP-адрес {name}: {new_ip}'
+    send_message(data)
--- system_monitor.py
+++ system_monitor.py
@@ -1,128 +1,13 @@
-import requests
-import configparser
 import logging
 import time
 
-from monitor_classes import Monitor
-
-MAX_TRY = 15
-
-
-def load_config():
-    config = configparser.ConfigParser()
-    config.read('system_monitor.ini', encoding='utf-8')
-    return config
-
-
-def ini_save(config_inf):
-    '''
-    Сохранение изменений в іnі-файл
-    '''
-    with open(f'system_monitor.ini', 'w') as f:
-        config_inf.write(f)
-
-
-conf = load_config()
-
-
-def send_message(data: dict):
-    """
-    Отправка сообщения админу
-
-    """
-    current_try = 0
-    while current_try < MAX_TRY:
-        current_try += 1
-        try:
-            requests.post(conf.get('URL', 'message_server_address') + 'telegram',
-                          data=data,
-                          headers={'Connection': 'close'})
-        except Exception as exc:
-            logging.exception(exc)
-        else:
-            logging.info('Send successful')
-            break
-    logging.error('Max try exceeded')
-
-
-def check_temperature():
-    """
-    Проверка превышения заданной температуры
-    """
-
-    max_temp = int(conf.get('MAX_VALUES', 'max_temp'))
-
-    temp = Monitor.get_temperature()
-    if temp >= max_temp and Monitor.block_message.get('temperature', False) is False:
-        data = dict()
-        data['to'] = conf.get('CONTACT', 'telegram_name')
-        name = Monitor.get_name_of_machine()
-        data['text'] = f'Температура {name} сейчас: {temp}°C'
-        send_message(data)
-        logging.info('Send successful')
-        Monitor.block_message['temperature'] = True
-    elif temp < max_temp:
-        Monitor.block_message['temperature'] = False
-
-
-def check_ssh_connections():
-    """
-    Проверка ssh соединений
-    """
-    connections = Monitor.get_ssh_connections()
-    for conn in Monitor.block_message['ssh'].keys():
-        if conn not in connections:
-            Monitor.block_message['ssh'].pop(conn)
-    for conn in connections:
-        if Monitor.block_message.get('ssh', {}).get(conn, False) is False:
-            data = dict()
-            data['to'] = conf.get('CONTACT', 'telegram_name')
-            name = Monitor.get_name_of_machine()
-            data['text'] = str(f'Новое подключение к "{name}" с адреса {conn}.'
-                               f'Текущее количество подключений: {len(connections)}')
-            send_message(data)
-            Monitor.block_message['ssh'][conn] = True
-        elif len(connections) == 0:
-            Monitor.block_message['ssh'].clear()
-
-
-def check_used_space():
-    """
-    Проверка оставшегося свободного места на жестком диске
-    """
-    max_used_space = int(conf.get('MAX_VALUES', 'max_used_space'))
-    used_space = Monitor.get_used_space()
-    name = Monitor.get_name_of_machine()
-    for key, value in used_space.items():
-        if value > max_used_space and Monitor.block_message.get('used_space').get(key, False) is False:
-            data = dict()
-            data['to'] = conf.get('CONTACT', 'telegram_name')
-            data['text'] = f'Свободное место в "{key}" на сервере {name} заканчивается. Осталось {100 - value}%'
-            send_message(data)
-            Monitor.block_message['used_space'][key] = True
-        elif value < max_used_space:
-            Monitor.block_message['used_space'][key] = False
-
-
-def check_ip():
-    """
-    Проверка изменения IP-адреса
-    """
-    old_ip = None
-    if conf.has_option('SETTINGS', 'self_ip'):
-        old_ip = conf.get('SETTINGS', 'self_ip')
-    new_ip = Monitor.get_self_ip()
-    if old_ip != new_ip or old_ip is None:
-        conf.set('SETTINGS', 'self_ip', new_ip)
-        ini_save(conf)
-    else:
-        if new_ip == old_ip:
-            return
-    name = Monitor.get_name_of_machine()
-    data = dict()
-    data['to'] = conf.get('CONTACT', 'telegram_name')
-    data['text'] = f'Новый IP-адрес {name}: {new_ip}'
-    send_message(data)
+from ini_service import load_config
+from check_functions import (
+    check_ip,
+    check_temperature,
+    check_ssh_connections,
+    check_used_space,
+)
 
 
 if __name__ == "__main__":
