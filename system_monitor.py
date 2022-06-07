adcb897e859d90efe86c655b6b3c4eda0065279f
vallenov <a2lenov@mail.ru>
2022-06-08 01:18:52 +0300

    Добавлен отдетьный процесс с проверками и app.run

--- system_monitor.py
+++ system_monitor.py
@@ -1,41 +1,18 @@
 import logging
-import time
 import threading
+import os
 
-from ini_service import load_config
-from check_functions import (
-    check_ip,
-    check_temperature,
-    check_ssh_connections,
-    check_used_space,
-)
-
-
-def check_loop():
-    i = 0
-    sec = 1
-    minute = sec * 60
-
-    while True:
-        i = 0 if i > 600 else i
-        try:
-            if i and not i % 5 * sec:
-                check_ip()
-                check_ssh_connections()
-            if i and not i % minute:
-                check_temperature()
-            if i and not i % 5 * minute:
-                check_used_space()
-        except Exception as _ex:
-            logging.exception(f'Unrecognized exception {_ex}')
-        i += 1
-        time.sleep(1)
+from flask_app import app
+from check_functions import check_loop
 
 
 if __name__ == "__main__":
     logging.basicConfig(filename='run.log',
                         level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s')
-    cl = threading.Thread(target=check_loop)
-    cl.start()
+    # хинт, что бы процессы не двоились
+    if 'WERKZEUG_RUN_MAIN' not in os.environ:
+        cl = threading.Thread(target=check_loop)
+        cl.start()
+    app.run(host='0.0.0.0', port=5112, debug=True, threaded=True)
 
