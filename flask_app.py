c7031c3e192befabafa444ca8103bf9b5a2bd346
vallenov <a2lenov@mail.ru>
2022-06-09 01:12:00 +0300

    Добавлены остановка, запуск и перезапуск ngrok по запросу

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,31 @@ import os
 app = Flask(__name__)
 
 
+@app.route('/start_ngrok', methods=['GET'])
+def restart_ngrok():
+    os.system('systemctl start ngrok.service')
+    res = {
+        'res': 'OK',
+        'msg': 'Ngrok запущен'
+    }
+    return res
+
+
 @app.route('/restart_ngrok', methods=['GET'])
 def restart_ngrok():
     os.system('systemctl restart ngrok.service')
-    return {'res': 'OK'}
+    res = {
+        'res': 'OK',
+        'msg': 'Ngrok перезапущен'
+    }
+    return res
+
+
+@app.route('/stop_ngrok', methods=['GET'])
+def restart_ngrok():
+    os.system('systemctl stop ngrok.service')
+    res = {
+        'res': 'OK',
+        'msg': 'Ngrok остановлен'
+    }
+    return res
