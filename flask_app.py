afb9cdcfb6c4f83e825b8b3d2876e24f68108164
vallenov <a2lenov@mail.ru>
2022-06-09 23:51:19 +0300

    Добавлены роуты для ngrok_db

--- flask_app.py
+++ flask_app.py
@@ -32,3 +32,33 @@ def restart_ngrok():
         'msg': 'Ngrok остановлен'
     }
     return res
+
+
+@app.route('/start_ngrok_db', methods=['GET'])
+def restart_ngrok_db():
+    os.system('systemctl start ngrok_db.service')
+    res = {
+        'res': 'OK',
+        'msg': 'Ngrok DB запущен'
+    }
+    return res
+
+
+@app.route('/restart_ngrok_db', methods=['GET'])
+def restart_ngrok_db():
+    os.system('systemctl restart ngrok_db.service')
+    res = {
+        'res': 'OK',
+        'msg': 'Ngrok DB перезапущен'
+    }
+    return res
+
+
+@app.route('/stop_ngrok_db', methods=['GET'])
+def restart_ngrok_db():
+    os.system('systemctl stop ngrok_db.service')
+    res = {
+        'res': 'OK',
+        'msg': 'Ngrok DB остановлен'
+    }
+    return res
