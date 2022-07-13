65e85120445d1c676baaadfacf1ef8b27e9d2c1d
vallenov <a2lenov@mail.ru>
2022-07-14 00:25:12 +0300

    Объединение функций + добавлена функция теста

--- flask_app.py
+++ flask_app.py
@@ -2,67 +2,29 @@ from flask import Flask
 from flask_restful import request
 import os
 import subprocess as sp
+import time
+
 from monitor import Monitor
 
 app = Flask(__name__)
 
 
-@app.route('/ngrok_start', methods=['GET'])
-def ngrok_start():
-    os.system('systemctl start ngrok.service')
+@app.route('/ngrok_<act>', methods=['GET'])
+def ngrok_ssh(action):
+    os.system(f'systemctl {action} ngrok.service')
     res = {
         'res': 'OK',
-        'msg': 'Ngrok запущен'
+        'msg': f'Ngrok {action}'
     }
     return res
 
 
-@app.route('/ngrok_restart', methods=['GET'])
-def ngrok_restart():
-    os.system('systemctl restart ngrok.service')
+@app.route('/ngrok_db_<action>', methods=['GET'])
+def ngrok_db(action):
+    os.system(f'systemctl {action} ngrok_db.service')
     res = {
         'res': 'OK',
-        'msg': 'Ngrok перезапущен'
-    }
-    return res
-
-
-@app.route('/ngrok_stop', methods=['GET'])
-def ngrok_stop():
-    os.system('systemctl stop ngrok.service')
-    res = {
-        'res': 'OK',
-        'msg': 'Ngrok остановлен'
-    }
-    return res
-
-
-@app.route('/ngrok_db_start', methods=['GET'])
-def ngrok_db_start():
-    os.system('systemctl start ngrok_db.service')
-    res = {
-        'res': 'OK',
-        'msg': 'Ngrok DB запущен'
-    }
-    return res
-
-
-@app.route('/ngrok_db_restart', methods=['GET'])
-def ngrok_db_restart():
-    os.system('systemctl restart ngrok_db.service')
-    res = {
-        'res': 'OK',
-        'msg': 'Ngrok DB перезапущен'
-    }
-    return res
-
-
-@app.route('/ngrok_db_stop', methods=['GET'])
-def ngrok_db_stop():
-    os.system('systemctl stop ngrok_db.service')
-    res = {
-        'res': 'OK',
-        'msg': 'Ngrok DB остановлен'
+        'msg': f'Ngrok DB {action}'
     }
     return res
 
@@ -111,3 +73,9 @@ def systemctl():
     return {
         'msg': f'К сервису {service} применена команда {action}'
     }
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    time.sleep(10)
+    return {'msg': 'OK'}
