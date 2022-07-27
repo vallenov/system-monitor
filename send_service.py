60d196700092df4ba7d92470978f6d6da0ca6130
vallenov <a2lenov@mail.ru>
2022-07-28 01:12:07 +0300

    Добавлена функции отправки сообщения с подтверждением

--- send_service.py
+++ send_service.py
@@ -1,7 +1,10 @@
 import logging
 import requests
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from ini_service import load_config
+from monitor import Monitor
 
 MAX_TRY = 15
 
@@ -24,3 +27,15 @@ def send_message(data: dict):
             logging.info('Send successful')
             return
     logging.error('Max try exceeded')
+
+
+def send_confirmation_message():
+    conf = load_config()
+    bot = telebot.TeleBot(conf.get('TELEBOT', 'token'))
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton("✅ Allow/Подтвердить", callback_data="allow_connection"))
+    bot.send_message(conf.get('TELEBOT', 'root_id'),
+                     f'Попытка нового подключения к {Monitor.get_name_of_machine()}',
+                     reply_markup=markup)
+
