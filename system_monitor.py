7e6b483a7be84e47896b3195a4ba36f8f1dd8b10
vallenov <a2lenov@mail.ru>
2022-08-04 01:32:55 +0300

    Убран лишний код

--- system_monitor.py
+++ system_monitor.py
@@ -1,12 +1,10 @@
 import logging
 
 from flask_app import app
-from check_functions import Checker
 
 if __name__ == "__main__":
     logging.basicConfig(filename='run.log',
                         level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s')
-    #Checker.run()
     app.run(host='0.0.0.0', port=5112, debug=True, threaded=True, use_reloader=False)
 
