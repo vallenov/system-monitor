1911524b5d151ac80098adb1f675c673f42d7896
vallenov <a2lenov@mail.ru>
2022-08-11 20:38:53 +0300

    Изменено логирование

--- system_monitor.py
+++ system_monitor.py
@@ -5,6 +5,6 @@ from flask_app import app
 if __name__ == "__main__":
     logging.basicConfig(filename='run.log',
                         level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     app.run(host='0.0.0.0', port=5112, debug=True, threaded=True, use_reloader=False)
 
