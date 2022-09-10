85cdedb30eba96f9538a8c4316919656ddbe0759
vallenov <a2lenov@mail.ru>
2022-09-10 12:51:10 +0300

    Добавлена надстройка над сервисами

---
+++ services/services_task.py
@@ -0,0 +1,29 @@
+import logging
+import time
+import threading
+import os
+
+from helpers import datetime_dict
+from services.check_metrics import Checker
+
+
+class ServicesTask:
+    @staticmethod
+    def main_loop():
+        """
+        Главный цикл проверки
+        """
+        while True:
+            try:
+                now_dict = datetime_dict()
+                Checker.run(now_dict=now_dict)
+            except Exception as _ex:
+                logging.exception(f'Unrecognized exception {_ex}')
+            time.sleep(1)
+
+    @staticmethod
+    def run():
+        # хинт, что бы процессы не двоились
+        if 'WERKZEUG_RUN_MAIN' not in os.environ:
+            mlt = threading.Thread(target=ServicesTask.main_loop)
+            mlt.start()
