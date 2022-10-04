cfef9d953575435f9ce855ba74b8505490573415
vallenov <a2lenov@mail.ru>
2022-10-04 20:41:59 +0300

    Добавлен модуль логирования

---
+++ loggers.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def get_logger(name: str, file_name='run.log'):
+    logger = logging.getLogger(name)
+    handler = logging.FileHandler(file_name)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+    return logger
