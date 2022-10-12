9a6df03c3cff8e347ce3605d2ebe02d18ee8fa82
vallenov <a2lenov@mail.ru>
2022-10-12 16:18:47 +0300

    Test

--- system_monitor.py
+++ system_monitor.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     logging.basicConfig(filename='run.log',
                         level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    os.system('systemctl stop ndrok_db.service')
-    os.system('systemctl start ndrok.service')
+    os.system('systemctl stop ngrok_db.service')
+    os.system('systemctl start ngrok.service')
     app.run(host=config.FlaskSettings.host,
             port=config.FlaskSettings.port,
             debug=True,
