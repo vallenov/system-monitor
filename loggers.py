0b707edd541066e108dd7be149273924f8533e44
vallenov <a2lenov@mail.ru>
2022-10-21 23:10:05 +0300

    Мелкая правка

--- loggers.py
+++ loggers.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def get_logger(name: str, file_name: str='run.log'):
+def get_logger(name: str, file_name: str = 'run.log'):
     logger = logging.getLogger(name)
     handler = logging.FileHandler(file_name)
     handler.setLevel(logging.INFO)
