f142299d2ee6f75497217f355a2a7630cb76186d
vallenov <a2lenov@mail.ru>
2022-10-22 23:57:17 +0300

    Устранение проблем с логированием

--- loggers.py
+++ loggers.py
@@ -1,16 +1,9 @@
 import logging
-import os
 
 logging.basicConfig(level=logging.INFO)
 
 
 def get_logger(name: str, file_name: str = 'run.log'):
-    curdir = os.curdir
-    if not os.path.exists(os.path.join(curdir, 'services')):
-        os.mkdir(os.path.join(curdir, 'services'))
-        os.chown(os.path.join(curdir, 'services'), 1000, 1000)
-        os.system(f'touch {os.path.join(curdir, "services", "deploy.log")}')
-        os.system(f'chmod 755 {os.path.join(curdir, "services", "deploy.log")}')
     logger = logging.getLogger(name)
     handler = logging.FileHandler(file_name)
     handler.setLevel(logging.INFO)
