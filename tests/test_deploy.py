39f8305f557e9d078950ad99f9b18c614f09a242
vallenov <a2lenov@mail.ru>
2022-10-24 00:18:05 +0300

    Добавлен тест

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -4,7 +4,7 @@ from services.deploy import Deploy
 def test_self_deploy():
     try:
         Deploy.deploy_project('system-monitor', 'system_monitor')
-    except Exception:
-        assert False, 'Ошибка'
+    except Exception as e:
+        assert False, f'Ошибка {e}'
     else:
         assert True
